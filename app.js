cfff51968f6538850e82a5f0c2fa3334e35c62e8
rodrigozaldivar <rodrigo@teracom.mx>
2024-02-27 18:26:45 -0600

    servidor con variables de entorno

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require("path");
 
 const app = express();
 
+const port = process.env.PORT || 3333;
+
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
@@ -25,4 +27,4 @@ app.post("/registro.html", (req, res) => {
 	res.sendFile(path.resolve(__dirname, "./view/home.html"));
 });
 
-app.listen(3333, () => console.log("Servidor corriendo"));
+app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`));
